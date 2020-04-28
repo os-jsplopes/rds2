0fc1bdae7683545b73e0de2b09db7512aa60d7a8
Júlio Lopes <julio.lopes@outsystems.com>
2020-04-28 10:29:55 +0100

    u1

--- .github/actions/ms.js
+++ .github/actions/ms.js
@@ -1,21 +1,25 @@
 const { Octokit } = require("@octokit/action");
 const octokit = new Octokit();
 
-const res = await octokit.graphql(
-    `query($owner:String!, $reponame:String!) {
+main();
+
+async function main() {
+    const res = await octokit.graphql(
+        `query($owner:String!, $reponame:String!) {
         repository(owner:$owner, name:$reponame) {
            packages(names: [$reponame], last: 1) {
             totalCount
           }
         }
       }`,
-    {
-        owner: "os-jsplopes",
-        reponame: "rds2",
-        headers: {
-            Accept: "application/vnd.github.packages-preview+json",
-        },
-    }
-);
+        {
+            owner: "os-jsplopes",
+            reponame: "rds2",
+            headers: {
+                Accept: "application/vnd.github.packages-preview+json",
+            },
+        }
+    );
 
-console.log(JSON.stringify(res, null, 2));
+    console.log(JSON.stringify(res, null, 2));
+}
